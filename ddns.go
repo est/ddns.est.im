1be7ce14218e9464f6a7f01501562116cc581715
est <electronicstar@126.com>
2014-01-15 16:27:42 +0800

    capable of recieving

--- ddns.go
+++ ddns.go
@@ -7,9 +7,12 @@ import (
 )
 
 func main() {
-    var buf []byte
-    svr_addr, _ := net.ResolveUDPAddr("udp", ":5353")
-    server, _ := net.ListenUDP("udp", svr_addr)
-    c, addr, err := server.ReadFrom(buf)
-    fmt.Println("done", c, addr, err)
+    var buf [512]byte
+    svr_addr, _ := net.ResolveUDPAddr("udp", "0:15353")
+    // up_addr, _ := net.ResolveUDPAddr("udp", "8.8.8.8:53")
+    server, err := net.ListenUDP("udp", svr_addr)
+    fmt.Println("server ok == ", err==nil)
+    c, addr, err := server.ReadFrom(buf[:512])
+    data := buf[:c]
+    fmt.Println("done", data, c, addr, buf, len(buf), cap(buf), err)
 }
