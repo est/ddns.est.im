91b37b6ced51070d20d693b5c8bd8c85933db397
est <electronicstar@126.com>
2014-01-15 16:54:22 +0800

    relay work done

--- ddns.go
+++ ddns.go
@@ -8,12 +8,20 @@ import (
 
 func main() {
     var buf [512]byte
-    svr_addr, _ := net.ResolveUDPAddr("udp", "0:15353")
-    up_addr, _ := net.ResolveUDPAddr("udp", "8.8.8.8:53")
+    var up_buf [512]byte
+    svr_addr, _ := net.ResolveUDPAddr("udp", "0:53")
     server, err1 := net.ListenUDP("udp", svr_addr)
-    up_server, err2 := net.DialUDP("UDP", nil, up_addr)
-    fmt.Println("server ok == ", err==nil)
-    c, addr, err := server.ReadFrom(buf[:512])
-    data := buf[:c]
-    fmt.Println("done", data, c, addr, buf, len(buf), cap(buf), err)
+    // up_addr, _ := net.ResolveUDPAddr("udp", )
+    for{
+            up_server, err2 := net.Dial("udp", "8.8.8.8:53")
+            fmt.Println("server ok == ", err1, err2)
+            
+            c, addr, _ := server.ReadFrom(buf[:512])
+            fmt.Println("L Received", buf[:c])
+
+            up_server.Write(buf[:c])
+            c, _ = up_server.Read(up_buf[:512])
+            server.WriteTo(up_buf[:c], addr)
+            fmt.Println("R Received", up_buf[:c])
+    }
 }
