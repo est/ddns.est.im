da22d837b06fd97ff18d7bedae437fdeb1285d32
est <electronicstar@126.com>
2016-05-29 17:51:37 +0800

    add cache

---
+++ funkyns/caching.py
@@ -0,0 +1,29 @@
+# coding: utf8
+
+import time
+
+
+class ExpiringCache(object):
+    _caches = {}
+    _timeouts = {}
+
+    def __init__(self, timeout=2):
+        self.timeout = timeout
+
+    def __call__(self, f):
+        self.cache = self._caches[f] = {}
+        self._timeouts[f] = self.timeout
+
+        def func(*args, **kwargs):
+            kw = sorted(kwargs.items())
+            key = (args, tuple(kw))
+            try:
+                v = self.cache[key]
+                if (time.time() - v[1]) > self.timeout:
+                    raise KeyError
+            except KeyError:
+                v = self.cache[key] = f(*args, **kwargs), time.time()
+            return v[0]
+        func.func_name = f.__name__
+
+        return func
